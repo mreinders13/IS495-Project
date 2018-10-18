ab524ff77226ae8bbed2223034920213162b2eac
Steven Rubio <stevenrubio@nevada.unr.edu>
2018-10-17 17:37:02 -0700

    I can kind of use github.

--- IS495-Project/Form1.Designer.vb
+++ IS495-Project/Form1.Designer.vb
@@ -23,23 +23,36 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.lblMichaelReinders = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'lblMichaelReinders
         '
         Me.lblMichaelReinders.AutoSize = True
-        Me.lblMichaelReinders.Location = New System.Drawing.Point(13, 13)
+        Me.lblMichaelReinders.Location = New System.Drawing.Point(10, 11)
+        Me.lblMichaelReinders.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.lblMichaelReinders.Name = "lblMichaelReinders"
-        Me.lblMichaelReinders.Size = New System.Drawing.Size(121, 17)
+        Me.lblMichaelReinders.Size = New System.Drawing.Size(92, 13)
         Me.lblMichaelReinders.TabIndex = 0
         Me.lblMichaelReinders.Text = "Michael Reinders "
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(72, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Steven Rubio"
+        '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(600, 366)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.lblMichaelReinders)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
@@ -48,4 +61,5 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents lblMichaelReinders As Label
+    Friend WithEvents Label1 As Label
 End Class
