3f0253f881868d46a075bb1d6bc3f5c05e57558b
mreinders13 <michael.h.reinders@gmail.com>
2018-10-17 19:14:44 -0700

    Added middle initial

--- IS495-Project/Form1.Designer.vb
+++ IS495-Project/Form1.Designer.vb
@@ -29,30 +29,30 @@ Partial Class Form1
         'lblMichaelReinders
         '
         Me.lblMichaelReinders.AutoSize = True
-        Me.lblMichaelReinders.Location = New System.Drawing.Point(10, 11)
-        Me.lblMichaelReinders.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblMichaelReinders.Location = New System.Drawing.Point(13, 14)
         Me.lblMichaelReinders.Name = "lblMichaelReinders"
-        Me.lblMichaelReinders.Size = New System.Drawing.Size(92, 13)
+        Me.lblMichaelReinders.Size = New System.Drawing.Size(135, 17)
         Me.lblMichaelReinders.TabIndex = 0
-        Me.lblMichaelReinders.Text = "Michael Reinders "
+        Me.lblMichaelReinders.Text = "Michael H Reinders "
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(13, 28)
+        Me.Label1.Location = New System.Drawing.Point(17, 34)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(72, 13)
+        Me.Label1.Size = New System.Drawing.Size(93, 17)
         Me.Label1.TabIndex = 1
         Me.Label1.Text = "Steven Rubio"
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(600, 366)
+        Me.ClientSize = New System.Drawing.Size(800, 450)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.lblMichaelReinders)
-        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
