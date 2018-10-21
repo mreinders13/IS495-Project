a1e40e3bee311ccc1ce261a301ba18c618672039
Jen <jentruong1358@gmail.com>
2018-10-21 14:48:44 -0700

    Added txtUsername textbox and btnEntrySubmit button

---
+++ IS495-Project/frmUsername.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmUsername
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.btnEntrySubmit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Location = New System.Drawing.Point(12, 12)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(100, 20)
+        Me.txtUsername.TabIndex = 0
+        '
+        'btnEntrySubmit
+        '
+        Me.btnEntrySubmit.Location = New System.Drawing.Point(12, 38)
+        Me.btnEntrySubmit.Name = "btnEntrySubmit"
+        Me.btnEntrySubmit.Size = New System.Drawing.Size(75, 23)
+        Me.btnEntrySubmit.TabIndex = 1
+        Me.btnEntrySubmit.Text = "Button1"
+        Me.btnEntrySubmit.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage
+        Me.btnEntrySubmit.UseVisualStyleBackColor = True
+        '
+        'frmUsername
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.btnEntrySubmit)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Name = "frmUsername"
+        Me.Text = "frmUsername"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents btnEntrySubmit As Button
+End Class
---
+++ IS495-Project/frmUsername.vb
@@ -0,0 +1,3 @@
+﻿Public Class frmUsername
+
+End Class
