32e5559e922bcd2b47f1fa3951af028615d484d8
Steven Rubio <stevenrubio@nevada.unr.edu>
2018-10-22 19:56:42 -0700

    Added a global variable class with global CurrentSemester field.

---
+++ IS495-Project/GlobalVariables.vb
@@ -0,0 +1,15 @@
+﻿Public Class GlobalVariables
+    Public Shared CurrentSemester As String = Nothing
+    Private Sub New()
+    End Sub
+
+    Public Shared ReadOnly Property GetCurrentSemester() As String
+        Get
+            Return CurrentSemester
+        End Get
+    End Property
+
+    Public Shared Sub SetClientUserName(argCurrentSemester As String)
+        CurrentSemester = argCurrentSemester
+    End Sub
+End Class
