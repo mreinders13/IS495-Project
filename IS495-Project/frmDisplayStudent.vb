5c0f9aed03193b496c37db032a77e174f1145ddf
Chris Lee <Chris.lee1334@hotmail.com>
2018-10-26 17:49:49 -0700

    Updated Buttons to save string appropriately as Accepted, Bridged, or Denied.

--- IS495-Project/frmDisplayStudent.vb
+++ IS495-Project/frmDisplayStudent.vb
@@ -1,4 +1,7 @@
 ﻿Public Class frmDisplayStudent
+    Dim Status, Accepted, Denied, Bridged As String
+    Dim DecisionAccept, DecisionDeny, DecisionBridge As Boolean
+
     Private Sub btnSaveData_Click(sender As Object, e As EventArgs) Handles btnSaveData.Click
 
     End Sub
@@ -9,14 +12,32 @@
 
     Private Sub btnAccept_Click(sender As Object, e As EventArgs) Handles btnAccept.Click
         'Save Accept decision
+        Status = Accepted
+        'These are for verification if needed later, we can delete the booleans if they are unfit for the scope
+        'DecisionAccept = True
+        'DecisionDeny = False
+        'DecisionBridge = False
 
     End Sub
 
     Private Sub btnBridge_Click(sender As Object, e As EventArgs) Handles btnBridge.Click
         'Save Bridge Decision
+        Status = Bridged
+        'These are for verification if needed later, we can delete the booleans if they are unfit for the scope
+        'DecisionBridge = True
+        'DecisionAccept = False
+        'DecisionDeny = False
+
     End Sub
 
     Private Sub btnDeny_Click(sender As Object, e As EventArgs) Handles btnDeny.Click
-        'Save Deny Decision
+        'Save Deny Decision 
+        Status = Denied
+        'These are for verification if needed later, we can delete the booleans if they are unfit for the scope
+        'DecisionBridge = False
+        'DecisionAccept = False
+        'DecisionDeny = True
+
+
     End Sub
 End Class
